2105ecc132b9e3fb5e2ebfe5fb2ba88042983000
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-05-28 12:34:21 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import Signup from "./components/pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/sign-up" element={<Signup />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HeroSection from "../HeroSection";
+import "../../App.css";
+import Cards from "../Card";
+import Footer from "../Footer";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
